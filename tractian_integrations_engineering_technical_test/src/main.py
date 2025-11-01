70919e7a440456ed3889ea51b6f9846943e36196
Gabriel L. A. Dutra <gabriel_dutra497@hotmail.com>
2025-10-31 23:09:37 -0300

    Improving logs and messages.

--- tractian_integrations_engineering_technical_test/src/main.py
+++ tractian_integrations_engineering_technical_test/src/main.py
@@ -20,7 +20,7 @@ async def inbound_flow():
         logger.info("Nenhum arquivo encontrado.")
         return None
     
-    logger.debug(f"Processando {len(files_data)} arquivo(s) encontrados.")
+    logger.debug(f"Processando {len(files_data)} workorder(s) encontrados.")
     for client_data in files_data:
         try:
             if not client_adapter.validate_client_data(client_data):
@@ -42,21 +42,18 @@ async def outbound_flow():
     
     workorders = await tracos_adapter.read_unsynced_workorders()
     if not workorders:
-        logger.info("Nenhuma workorder não sincronizada encontrada")
-        return
+        return None
     
-    logger.info(f"Processando {len(workorders)} workorder(s)")
+    logger.debug(f"Processando {len(workorders)} workorder(s) encontrados.")
+
     for tracos_data in workorders:
         try:
-            workorder_number = tracos_data.get('number')
+            client_data = data_translator.convert_tracos_to_client(tracos_data)
             
-            client_data = data_translator.tracos_to_client(tracos_data)
-            
+            workorder_number = tracos_data['number']
             filename = f"workorder_{workorder_number}.json"
             success = client_adapter.write_outbound_file(filename, client_data)
-            
             if success:
-                # Marca como sincronizada
                 await tracos_adapter.mark_workorder_as_synced(workorder_number)
                 
         except Exception as e:
@@ -64,20 +61,21 @@ async def outbound_flow():
 
 
 async def main():
-    logger.info("=============== INICIANDO PIPELINE DE INTEGRAÇÃO ===============")
     
     try:
+        logger.info("=============== INICIANDO PIPELINE DE INTEGRAÇÃO ===============")
+
         await inbound_flow()
         await outbound_flow()
-        
-        # Cleanup
         await tracos_adapter.close_connection()
         
         logger.info("=============== PIPELINE CONCLUÍDO COM SUCESSO ===============")
         
     except Exception as e:
-        logger.error(f"Erro no pipeline: {e}")
+        logger.error(f"Falha crítica na execução do pipeline de integração: {e}", exc_info=True)
         await tracos_adapter.close_connection()
+        raise
+
 
 
 if __name__ == "__main__":
